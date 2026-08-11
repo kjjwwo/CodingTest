9248e141c7ec513c1064025520df1f8c3149386c
kjjwwo <kimjongwo11@gmail.com>
2026-08-11 23:50:01 +0900

    26.08.11 coding test study01

--- src/programmers/Lv1/명예의전당1/Solution.java
+++ src/programmers/Lv1/명예의전당1/Solution.java
@@ -1,11 +1,21 @@
 package programmers.Lv1.명예의전당1;
 
-import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class Solution {
     public int[] solution(int k, int[] score) {
     	int[] answer = new int[score.length];
-
+    	
+    	PriorityQueue<Integer> record = new PriorityQueue<Integer>();
+    	
+    	for (int i = 0; i < score.length; i++) {
+			record.add(score[i]);
+			if (record.size() > k) {
+				record.poll();
+			}
+			answer[i] = record.peek();
+		}
+    	
     	return answer;
     }
 }
