1f84e4338f11fbfcb81add672173535a71ddb7cc
kjjwwo <kimjongwo11@gmail.com>
2026-09-20 23:17:09 +0900

    coding test study01

--- src/com/ssafy/assignment/swea1868/Solution.java
+++ src/com/ssafy/assignment/swea1868/Solution.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
 /**
  * 
@@ -13,28 +12,43 @@ import java.util.StringTokenizer;
  * @author JW
  * 
  * NxN 크기의 맵
+ * N(1 ≤ N ≤ 300) 
  * int[][] map = new int[N][N]
- * 기본 0으로 해두고 지뢰는 -1, 지뢰 만나면 주변에 +1하기
- * 
- * cnt = 0;
- * 
- * 전체를 탐색 -> 반복문으로 돌다가 0을 만나면
- * bfs로 탐색하면서 0을 만나지 않을 때까지 탐색 진행
- * 0을 찾고 그 주변지뢰는 모두 0으로 작성
- * 탐색 끝나면 cnt++
- * 
- * 전체 맵을 탐색하면
- * 다시 한번더 전체 맵을 돌면서 0보다 큰 값을 모두 더해줌
+ * map[r][c]
+ *  -1: 지뢰
+ *   0: 주변 지뢰 없음
+ * 1~8: 주변 지뢰 개수
  * 
  * 
+ * 1. 각 칸 주변의 지뢰 개수 계산
+ * int[] dr = {-1,-1,-1,0,0,1,1,1}
+ * int[] dc = {-1,0,1,-1,1,-1,0,1}
+ * 
+ * 
+ * 2. 모든 0을 찾아 BFS
+ * for(int r = 0; r < N; r++)
+ * 	for(int c = 0; c < N; c++)
+ * 		if(map[r][c] == 0 && !visited[r][c])
+ * 			bfs(r,c);
+ * 			cnt++;
+ * 
+ * visited로 방문 여부 관리 map이랑 똑같이 생긴 걸로 (boolean)
+ * 0이 아닌 숫자칸도 visited 처리
+ * 하지만 queue에는 0만 넣음
+ * 
+ * 3. BFS로 0 탐색 끝나고 아직 안 열린 칸 계산
  * 
  */
 
 public class Solution {
+	
 	private static int[][] map;
+	private static boolean[][] visited;
+	
 	private static int[] dr = {-1,-1,-1,0,0,1,1,1};
 	private static int[] dc = {-1,0,1,-1,1,-1,0,1};
 	private static int cnt;
+	private static int N;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -42,10 +56,12 @@ public class Solution {
 		int T = Integer.parseInt(br.readLine());
 		
 		for (int tc = 1; tc <= T; tc++) {
-			int N = Integer.parseInt(br.readLine());
+			N = Integer.parseInt(br.readLine());
 			
+			cnt = 0;
 			map = new int[N][N];
-			
+			visited = new boolean[N][N];
+			// map 입력 받기
 			for (int r = 0; r < N; r++) {
 				String str = br.readLine();
 				for (int c = 0; c < N; c++) {
@@ -54,54 +70,73 @@ public class Solution {
 					}
 				}
 			}
+			
+			// map 설정
 			for (int r = 0; r < N; r++) {
 				for (int c = 0; c < N; c++) {
-					if(map[r][c] == -1) continue;
+					if(map[r][c] == -1) continue; // 지뢰가 있으면 패스
+					// 0이면 지뢰 갯수 체크
+					int mine = 0;
 					for (int i = 0; i < 8; i++) {
-						int nr = r + dr[i];
-						int nc = c + dc[i];
-						if(nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
-						if(map[nr][nc] == -1) {
-							map[r][c]++;
+						if(r+dr[i] < 0 || r+dr[i] >= N || c+dc[i] < 0 || c+dc[i] >= N) continue;
+						if(map[r+dr[i]][c+dc[i]] == -1) {
+							mine++;
 						}
 					}
-					
+					map[r][c] = mine;
 				}
 			}
 			
-			
-			cnt = 0;
+			// 0인 부분 확인하고 열기
 			for (int r = 0; r < N; r++) {
 				for (int c = 0; c < N; c++) {
-					if(map[r][c] == 0) {
+					if(map[r][c] == 0 && !visited[r][c]) {
 						bfs(r,c);
 						cnt++;
 					}
 				}
 			}
+
 			for (int r = 0; r < N; r++) {
 				for (int c = 0; c < N; c++) {
-					if(map[r][c] > 0) {
+					if(map[r][c] != -1 && !visited[r][c]) {
 						cnt++;
 					}
 				}
 			}
-			
-			sb.append("#").append(tc).append(" ").append(cnt);
+			sb.append("#").append(tc).append(" ").append(cnt).append("\n");
 		}
 		System.out.print(sb.toString());
 	}
 
-	// dfs가 좋지 않나? 한 길로 쭉 가다가 다 찾음녀 올라가고 흠... 아닌가
-	private static void bfs(int r, int c) {
-		// TODO Auto-generated method stub
+	private static void bfs(int row, int col) {
 		Queue<int[]> queue = new LinkedList<>();
 		
-		int[] coord = {0,0};
-		queue.add(coord);
+		queue.offer(new int[] {row,col});
+		visited[row][col] = true;
+		
 		while(!queue.isEmpty()) {
+			int[] coord = queue.poll();
+			int r = coord[0];
+			int c = coord[1];
 			
+			for (int i = 0; i < 8; i++) {
+				
+				int nr = r+dr[i];
+				int nc = c+dc[i];
+				
+				if(nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
+				
+				if(visited[nr][nc]) continue;
+				
+				if(map[nr][nc] == -1) continue;
+				
+				visited[nr][nc] = true;
+				
+				if(map[nr][nc] == 0) {
+					queue.offer(new int[] {nr, nc});
+				}
+			}
 		}
 	}
-
 }
--- src/com/ssafy/assignment/swea6782/Solution.java
+++ src/com/ssafy/assignment/swea6782/Solution.java
@@ -1,14 +1,18 @@
 package com.ssafy.assignment.swea6782;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * 
  * @author JW
  * 
- * 현주(이상함)가 좋아하는 제곱근 놀이
+ * 6782.현주(이상함)가 좋아하는 제곱근 놀이
  * 
  * 2이상의 정수 N (2 <= N <= 10^12) 1조....? 1_000_000_000_000
  * 10^3^4 ~ 2^10^4=2^40... 단위가 미쳤음.. 2의 40승, 1조까지 가능
- *  
+ * 
  * 
  * N에 할 수 있는 행동 2가지
  * 1. N = N+1;
@@ -16,26 +20,94 @@ package com.ssafy.assignment.swea6782;
  * N이 2가 될 때까지 반복
  * 이때 행동을 최소로 한다면 몇 번만에 가능?
  * 
- * Math.sqrt() // 이건 double 형태로 값 전달해줌 round한 값과 원래 값인 동일한지 판단 후 비교?
- * 
+ * 1.Math.sqrt() // 이건 double 형태로 값 전달해줌 round한 값과 원래 값인 동일한지 판단 후 비교?
+ * 1-1. 분기 필요 sqrt == (long) sqrt 이면, 그냥 cnt++하고 N = sqrt임
+ * 			이게 아니면 다음 값 계산 필요.
+ * 1-2. 현재 값을 sqrt 진행. 그걸 올림함. 
+ * 1-3. 올림한 값 제곱에서 현재값 빼서 cnt에 더함
+ * 1-4. 올림한 값을 또 sqrt하고 또 올림 및 제곱
  * 
  * 만일 N이 1조까지 가능하다면, 루트 N은 몇까지 가능? 10^6 = 1_000_000 백만
  * 
+ * binary tree
+ * parametric search 사용!!!
+ * 2. left, right 수를 정하고 mid를 구해서 이진 탐색 진행!!
+ * while(left <= right)
+ * 		mid = (left + right) / 2
+ * 		squred = mid * mid
+ * 		if(N <= squared): answer = mid, right = mid-1 // mid까지 확인했으니 한칸 앞으로 이동!
+ *		else : left = mid+1 // mid까지 확인했으니 한칸 뒤로 이동!
+ * return = answer
  * 
- * 
- * 
- *
  */
 
 public class Solution {
-	public static void main(String[] args) {
-		long num = 1_000_000_000_000L;
-		long[] square = new long[1_000_000];
-		for (int i = 0; i < 1_000_000; i++) {
-			square[i] = i^2;
+	private static long cnt;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		// 분기 필요 sqrt == (long) sqrt 이면, 그냥 cnt++하고 N = sqrt임
+		// 		  이게 아니면 다음 값 계산 필요.
+		// 현재 값을 sqrt 진행. 그걸 올림함. 
+		// 올림한 값 제곱에서 현재값 빼서 cnt에 더함
+		// 올림한 값을 또 sqrt하고 또 올림 및 제곱
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int TC = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= TC; tc++) {
+			long N = Long.parseLong(br.readLine());
+			cnt = 0;
+			
+			// 1. Math.sqrt() 사용
+//			while(N != 2) {
+//				double sqrtDouble = Math.sqrt(N);
+//				if(Math.round(sqrtDouble) == sqrtDouble ) {
+//					N = (long) sqrtDouble;
+//					cnt++;
+//					continue;
+//				}
+//				
+//				long sqrt = (long) Math.sqrt(N);
+//				long sqrtSquared = (sqrt + 1) * (sqrt + 1);
+//				cnt += (sqrtSquared - N);
+//				N = sqrt + 1;
+//				cnt++;
+//			}
+			
+			// 2. Parametric search 사용
+			while(N != 2) {
+				long sqrt = parametricSearch(N);
+				cnt += sqrt * sqrt - N;
+				N = sqrt;
+				cnt++;
+			}
+			
+			
+			sb.append("#").append(tc).append(" ").append(cnt).append("\n");
+		}
+		System.out.print(sb.toString());
+	}
+	
+	private static long parametricSearch(long n) {
+		long left = 2;
+		long right = 1_000_000L;
+		long answer = 0;
+		while(left <= right) {
+			long mid = left + (right - left) / 2; // ㅐoverflow 위험이 있으니 이렇게!!!
+			if(n <= mid * mid) {
+				answer = mid;
+				right = mid - 1;
+			}
+			else {
+				left = mid + 1;
+			}
 		}
 		
-		
-		
+		return answer;
 	}
+	
+	
 }
+ 
+
